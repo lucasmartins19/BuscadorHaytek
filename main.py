ad999edbe1f40a2938212daa09e9cdaf3e6ec363
Lucas <lucasmartinslmo15@gmail.com>
2023-12-19 17:12:52 -0300

    Adicionada coluna com data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import locale
+from datetime import datetime
 
 def main():
     empresas = usuario.verificar_empresas()
@@ -15,7 +16,7 @@ def main():
     layout_principal = [
         [sg.Text("Empresa:"), sg.Combo(values=empresas, default_value=empresas[0], key="empresa", enable_events=True, readonly=True, size=(30))],
         [sg.Input(size=(20), key="campo_pesquisa", readonly=True), sg.Button("Pesquisar", key="pesquisar", disabled=True)],
-        [sg.Table([], key="tabela", row_height=20, headings = ["Lente", "Nome", "OS","OD ESF.", "OD CIL", "OE ESF", "OE CIL", "AD OD", "AD OE", "PEDIDO", "VALOR"])],
+        [sg.Table([], key="tabela", row_height=20, headings = ["Lente", "Data", "Nome", "OS","OD ESF.", "OD CIL", "OE ESF", "OE CIL", "AD OD", "AD OE", "PEDIDO", "VALOR"])],
         [sg.Image(data=ring_gray_segments_big, pad=0, key="animacao_dados", visible=True)],
         [sg.Push(), sg.Text("0", key="Registros", pad=0)]
     ]
@@ -93,21 +94,21 @@ class Usuario:
             if len(requisicao) > 1:
                 for req in requisicao:
                     self.pedidos_l.append(req)
-                    self.window_principal.write_event_value("carregando", [req['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), req['NOME'], req['OSCLI'], req['DIR_ESFER'], req['DIR_CIL'], req['ESQ_ESFER'], req['ESQ_CIL'], req['DIR_ADD'], req['ESQ_ADD'], req['PEDIDO'], locale.currency(req['VALOR'], grouping=True)])
+                    self.window_principal.write_event_value("carregando", [req['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), self.pedidos[req['PEDIDO']], req['NOME'], req['OSCLI'], req['DIR_ESFER'], req['DIR_CIL'], req['ESQ_ESFER'], req['ESQ_CIL'], req['DIR_ADD'], req['ESQ_ADD'], req['PEDIDO'], locale.currency(req['VALOR'], grouping=True)])
             else:
                 self.pedidos_l.append(requisicao[0])
-                self.window_principal.write_event_value("carregando", [requisicao[0]['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), requisicao[0]['NOME'], requisicao[0]['OSCLI'], requisicao[0]['DIR_ESFER'], requisicao[0]['DIR_CIL'], requisicao[0]['ESQ_ESFER'], requisicao[0]['ESQ_CIL'], requisicao[0]['DIR_ADD'], requisicao[0]['ESQ_ADD'], requisicao[0]['PEDIDO'], locale.currency(requisicao[0]['VALOR'], grouping=True)])
+                self.window_principal.write_event_value("carregando", [requisicao[0]['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), self.pedidos[requisicao[0]['PEDIDO']], requisicao[0]['NOME'], requisicao[0]['OSCLI'], requisicao[0]['DIR_ESFER'], requisicao[0]['DIR_CIL'], requisicao[0]['ESQ_ESFER'], requisicao[0]['ESQ_CIL'], requisicao[0]['DIR_ADD'], requisicao[0]['ESQ_ADD'], requisicao[0]['PEDIDO'], locale.currency(requisicao[0]['VALOR'], grouping=True)])
         except:
             return self.funcao_auxiliar_requisicao(pedido)
 
     def extrair_dados_pedidos(self, window_principal):
         self.window_principal = window_principal
-        pedidos = self.lista_pedidos()
-        if pedidos is not None:
+        self.pedidos = self.lista_pedidos()
+        if self.pedidos is not None:
             self.pedidos_l = list()
             with ThreadPoolExecutor(max_workers=10) as pool:
-                pool.map(self.funcao_auxiliar_requisicao, pedidos)
-            self.pedidos_l_org = sorted([[pedido['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), pedido['NOME'], pedido['OSCLI'], pedido['DIR_ESFER'], pedido['DIR_CIL'], pedido['ESQ_ESFER'], pedido['ESQ_CIL'], pedido['DIR_ADD'], pedido['ESQ_ADD'], pedido['PEDIDO'], locale.currency(pedido['VALOR'], grouping=True)] for pedido in self.pedidos_l], reverse=True, key=lambda l: int(l[9].strip("MG")))
+                pool.map(self.funcao_auxiliar_requisicao, [pedido for pedido in self.pedidos])
+            self.pedidos_l_org = sorted([[pedido['DESCRICAO'].strip("Lente Haytek Visão Simples Acabada"), self.pedidos[pedido['PEDIDO']], pedido['NOME'], pedido['OSCLI'], pedido['DIR_ESFER'], pedido['DIR_CIL'], pedido['ESQ_ESFER'], pedido['ESQ_CIL'], pedido['DIR_ADD'], pedido['ESQ_ADD'], pedido['PEDIDO'], locale.currency(pedido['VALOR'], grouping=True)] for pedido in self.pedidos_l], reverse=True, key=lambda l: int(l[10].strip("MG")))
             self.window_principal.write_event_value("carregou", '')
 
     def lista_pedidos(self):
@@ -121,7 +122,9 @@ class Usuario:
                 "companies": [self.codigo_empresa]
             }
             pedidos = requests.post(f"https://api.haytek.com.br/v1.1/orders/history/v2/{self.codigo_empresa}", headers=self.headers, json=json).json()['RESULT']
-            return [pedido['Pedhtk'] for pedido in pedidos]
+            # return [[pedido['Pedhtk'], datetime.strptime(pedido['Data_pedido'], '%Y-%m-%d').strftime('%d/%m/%Y') ] for pedido in pedidos]
+            return { pedido['Pedhtk']: datetime.strptime(pedido['Data_pedido'], '%Y-%m-%d').strftime('%d/%m/%Y') for pedido in pedidos}
+        
         except Exception as excecao:
             print(excecao.__class__, excecao)
             return None
